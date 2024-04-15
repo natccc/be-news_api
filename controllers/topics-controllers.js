a8350b75c4c8ea1615a17393a4a83a81e9ea757a
Natalie <natchc@gmail.com>
2024-04-15 16:44:11 +0100

    add sort order to be date created desc, add details to endpoints.json

--- controllers/topics-controllers.js
+++ controllers/topics-controllers.js
@@ -1,4 +1,4 @@
-const { fetchTopics, fetchEndpoints, fetchArticleById } = require("../models/topics-models.js");
+const { fetchTopics, fetchEndpoints, fetchArticleById, fetchArticles } = require("../models/topics-models.js");
 
 exports.getTopics = (req, res, next) => {
   return fetchTopics()
@@ -21,6 +21,14 @@ exports.getArticleById = (req, res, next) => {
   return fetchArticleById(article_id)
     .then((article) => {
       res.status(200).send({article});
+    })
+    .catch(next);
+}
+
+exports.getArticles = (req, res, next) => {
+  return fetchArticles()
+    .then((articles) => {
+      res.status(200).send({articles});
     })
     .catch(next);
 }
