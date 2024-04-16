f4bbfa0c81b731110461dcb0b583e099f3da3352
Natalie <natchc@gmail.com>
2024-04-16 12:19:51 +0100

    add get users endpoint, the test and update /api

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -79,133 +79,6 @@ describe("/api/articles/:article_id", () => {
         expect(body.message).toBe("article_id not found");
       });
   });
-});
-describe("/api/articles", () => {
-  it("GET 200: Responds with an array of article objects", () => {
-    return request(app)
-      .get("/api/articles")
-      .expect(200)
-      .then(({ body: { articles } }) => {
-        expect(articles).toBeSortedBy("created_at", { descending: true });
-        expect(articles.length).toBe(13);
-        articles.forEach((article) => {
-          expect(article).toMatchObject({
-            title: expect.any(String),
-            topic: expect.any(String),
-            author: expect.any(String),
-            body: expect.any(String),
-            created_at: expect.any(String),
-            votes: expect.any(Number),
-            article_img_url: expect.any(String),
-            article_id: expect.any(Number),
-            comment_count: expect.any(String),
-          });
-        });
-      });
-  });
-});
-describe("/api/articles/:article_id/comments", () => {
-  it("GET 200: Responds with an array of comments associated with the article_id", () => {
-    return request(app)
-      .get("/api/articles/1/comments")
-      .expect(200)
-      .then(({ body: { comments } }) => {
-        expect(comments.length).toBe(11);
-        expect(comments).toBeSortedBy("created_at", { descending: true });
-        comments.forEach((comment) => {
-          expect(comment).toMatchObject({
-            comment_id: expect.any(Number),
-            body: expect.any(String),
-            votes: expect.any(Number),
-            author: expect.any(String),
-            created_at: expect.any(String),
-            article_id: expect.any(Number),
-          });
-        });
-      });
-  });
-  it('GET 200: Responds with an empty array when article_id exist but has no comments', () => {
-    return request(app)
-     .get("/api/articles/2/comments")
-      .expect(200)
-      .then(({ body: { comments } }) => {
-        expect(comments.length).toBe(0);
-      });
-  });
-  it("GET 404: Responds with an error when article_id is valid but non-existent", () => {
-    return request(app)
-      .get("/api/articles/100/comments")
-      .expect(404)
-      .then(({ body }) => {
-        expect(body.message).toBe("article_id not found");
-      });
-  });
-  it("GET 400: Responds with an error when article_id is of invalid type", () => {
-    return request(app)
-      .get("/api/articles/dog/comments")
-      .expect(400)
-      .then(({ body }) => {
-        expect(body.message).toBe("bad request");
-      });
-  });
-  
-  it("POST 201: Responds with the posted comment", () => {
-    return request(app)
-      .post("/api/articles/1/comments")
-      .send({
-        username: "butter_bridge",
-        body: "This is a test comment",
-      })
-      .expect(201)
-      .then(({ body: { comment } }) => {
-        expect(comment).toMatchObject({
-          comment_id: expect.any(Number),
-          body: "This is a test comment",
-          votes: 0,
-          author: "butter_bridge",
-          created_at: expect.any(String),
-          article_id: 1,
-        });
-      });
-  });
-  it("POST 404: Responds with an error when article_id is valid but non-existent", () => {
-    return request(app)
-      .post("/api/articles/100/comments")
-      .send({
-        username: "butter_bridge",
-        body: "This is a test comment",
-      })
-      .expect(404)
-      .then(({ body }) => {
-        expect(body.message).toBe("not found");
-      });
-  });
-  it("POST 400: Responds with an error when article_id is of invalid type", () => {
-    return request(app)
-      .post("/api/articles/dog/comments")
-      .send({
-        username: "butter_bridge",
-        body: "This is a test comment",
-      })
-      .expect(400)
-      .then(({ body }) => {
-        expect(body.message).toBe("bad request");
-      });
-  });
-  it("POST 400: Responds with en error when the response body is not of correct format (incorrect column name)", () => {
-    return request(app)
-      .post("/api/articles/1/comments")
-      .send({
-        banana: "butter_bridge",
-        body: "This is a test comment",
-      })
-      .expect(400)
-      .then(({ body }) => {
-        expect(body.message).toBe("bad request");
-      });
-  });
-});
-describe("/api/articles/:article_id", () => {
   it("PATCH 200: Updates the vote of the article associated with the article_id", () => {
     return request(app)
       .patch("/api/articles/1")
@@ -262,26 +135,166 @@ describe("/api/articles/:article_id", () => {
       });
   });
 });
-describe('/api/comments/:comment_id', () => {
-    it('DELETE 204: Delete a comment associated with the comment_id)', () => {
-        return request(app)
-         .delete('/api/comments/1')
-         .expect(204)
-    });
-    it('DELETE 404: Responds with an error when comment_id is valid but non-existent', () => {
-        return request(app)
-         .delete('/api/comments/100')
-         .expect(404)
-         .then(({ body }) => {
-              expect(body.message).toBe('comment_id not found')
-          })
-    });
-    it('DELETE 400: Responds with an error when comment_id is of invalid type', () => {
-        return request(app)
-         .delete('/api/comments/dog')
-         .expect(400)
-         .then(({ body }) => {
-              expect(body.message).toBe('bad request')
-          })
-    });
+describe("/api/articles", () => {
+  it("GET 200: Responds with an array of article objects", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body: { articles } }) => {
+        expect(articles).toBeSortedBy("created_at", { descending: true });
+        expect(articles.length).toBe(13);
+        articles.forEach((article) => {
+          expect(article).toMatchObject({
+            title: expect.any(String),
+            topic: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            article_id: expect.any(Number),
+            comment_count: expect.any(String),
+          });
+        });
+      });
+  });
+});
+describe("/api/articles/:article_id/comments", () => {
+  it("GET 200: Responds with an array of comments associated with the article_id", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then(({ body: { comments } }) => {
+        expect(comments.length).toBe(11);
+        expect(comments).toBeSortedBy("created_at", { descending: true });
+        comments.forEach((comment) => {
+          expect(comment).toMatchObject({
+            comment_id: expect.any(Number),
+            body: expect.any(String),
+            votes: expect.any(Number),
+            author: expect.any(String),
+            created_at: expect.any(String),
+            article_id: expect.any(Number),
+          });
+        });
+      });
+  });
+  it("GET 200: Responds with an empty array when article_id exist but has no comments", () => {
+    return request(app)
+      .get("/api/articles/2/comments")
+      .expect(200)
+      .then(({ body: { comments } }) => {
+        expect(comments.length).toBe(0);
+      });
+  });
+  it("GET 404: Responds with an error when article_id is valid but non-existent", () => {
+    return request(app)
+      .get("/api/articles/100/comments")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.message).toBe("article_id not found");
+      });
+  });
+  it("GET 400: Responds with an error when article_id is of invalid type", () => {
+    return request(app)
+      .get("/api/articles/dog/comments")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.message).toBe("bad request");
+      });
+  });
+
+  it("POST 201: Responds with the posted comment", () => {
+    return request(app)
+      .post("/api/articles/1/comments")
+      .send({
+        username: "butter_bridge",
+        body: "This is a test comment",
+      })
+      .expect(201)
+      .then(({ body: { comment } }) => {
+        expect(comment).toMatchObject({
+          comment_id: expect.any(Number),
+          body: "This is a test comment",
+          votes: 0,
+          author: "butter_bridge",
+          created_at: expect.any(String),
+          article_id: 1,
+        });
+      });
+  });
+  it("POST 404: Responds with an error when article_id is valid but non-existent", () => {
+    return request(app)
+      .post("/api/articles/100/comments")
+      .send({
+        username: "butter_bridge",
+        body: "This is a test comment",
+      })
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.message).toBe("not found");
+      });
+  });
+  it("POST 400: Responds with an error when article_id is of invalid type", () => {
+    return request(app)
+      .post("/api/articles/dog/comments")
+      .send({
+        username: "butter_bridge",
+        body: "This is a test comment",
+      })
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.message).toBe("bad request");
+      });
+  });
+  it("POST 400: Responds with en error when the response body is not of correct format (incorrect column name)", () => {
+    return request(app)
+      .post("/api/articles/1/comments")
+      .send({
+        banana: "butter_bridge",
+        body: "This is a test comment",
+      })
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.message).toBe("bad request");
+      });
+  });
+});
+describe("/api/comments/:comment_id", () => {
+  it("DELETE 204: Delete a comment associated with the comment_id)", () => {
+    return request(app).delete("/api/comments/1").expect(204);
+  });
+  it("DELETE 404: Responds with an error when comment_id is valid but non-existent", () => {
+    return request(app)
+      .delete("/api/comments/100")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.message).toBe("comment_id not found");
+      });
+  });
+  it("DELETE 400: Responds with an error when comment_id is of invalid type", () => {
+    return request(app)
+      .delete("/api/comments/dog")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.message).toBe("bad request");
+      });
+  });
+});
+describe("/api/users", () => {
+  it("GET 200: Responds with an array of all users ", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body: { users } }) => {
+        expect(users).toHaveLength(4);
+        users.forEach((user) => {
+          expect(user).toMatchObject({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
 });
