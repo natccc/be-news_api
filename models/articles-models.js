be76ffb4908c6d6c42c5ed9cd92451ac8db607db
Natalie <natchc@gmail.com>
2024-04-16 15:20:46 +0100

    add comment count into articles/:article_id, update the test, update /api

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,7 +1,11 @@
 const db = require("../db/connection.js");
 exports.fetchArticleById = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .query(`SELECT articles.*, COUNT(comment_id) AS comment_count FROM articles 
+    LEFT JOIN comments 
+    ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    HAVING articles.article_id = $1`, [article_id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({
