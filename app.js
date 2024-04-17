7ee193d37ead0a4123fd5c9b5da3c59bd4eeebcb
Natalie <natchc@gmail.com>
2024-04-17 10:05:51 +0100

    post articles

--- app.js
+++ app.js
@@ -1,42 +1,40 @@
-const express = require('express')
-const app = express()
-const apiRouter = require('./routes/api-router')
+const express = require("express");
+const app = express();
+const apiRouter = require("./routes/api-router");
 
-
-app.use(express.json())
-app.use('/api', apiRouter)
+app.use(express.json());
+app.use("/api", apiRouter);
 
 //error handling by express
-app.all('*',(req,res,next)=>{
-    res.status(404).send({message: "endpoint not found"})
-})
+app.all("*", (req, res, next) => {
+  res.status(404).send({ message: "endpoint not found" });
+});
 
 //psql errors
-app.use((err,req,res,next) => {
-    if(err.code==="23502" || err.code==="22P02"){
-     res.status(400).send({message: "bad request"})
-    }
-    next(err)
- })
+app.use((err, req, res, next) => {
+  if (err.code === "23502" || err.code === "22P02") {
+    res.status(400).send({ message: "bad request" });
+  }
+  next(err);
+});
 
- //posting comment to non existing id
- app.use((err,req,res,next)=>{
-    if(err.code==="23503"){
-        res.status(404).send({message: "not found"})
-    }
-    next(err)
-    })
+//posting comment to non existing id
+app.use((err, req, res, next) => {
+  if (err.code === "23503") {
+    res.status(404).send({ message: "not found" });
+  }
+  next(err);
+});
 
- //custom error handling
-app.use((err,req,res,next)=>{
-    if(err.status && err.message){
-        res.status(err.status).send({message: err.message})
-    }
-    next(err)
-})
-app.use((err,req,res,next)=>{
-    res.status(500).send({message: "internal server error"})
-    })
+//custom error handling
+app.use((err, req, res, next) => {
+  if (err.status && err.message) {
+    res.status(err.status).send({ message: err.message });
+  }
+  next(err);
+});
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: "internal server error" });
+});
 
-
-module.exports = app
+module.exports = app;
--- routes/articles-router.js
+++ routes/articles-router.js
@@ -1,8 +1,9 @@
 const router= require('express').Router()
-const{getArticleById, getArticles, patchArticle}=require('../controllers/articles-controllers')
+const{getArticleById, getArticles, patchArticle, postArticle}=require('../controllers/articles-controllers')
 const { getComments, postComment} = require('../controllers/comments-controllers')
 
 router.get('/', getArticles)
+router.post('/', postArticle)
 router.get('/:article_id', getArticleById)
 router.patch('/:article_id', patchArticle)
 router.get('/:article_id/comments', getComments)
